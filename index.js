24ba527cce8669934c05a90a6065a2a246a6a741
wangtiejun123 <1550611982@qq.com>
2019-02-24 13:58:48 +0800

    '新增注册登陆相关逻辑'

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.get('/user', function(req, res) {
     res.send('ok');
 });
 
+// 注册
+app.post('/register', function(req, res) {
+    res.send('注册');
+});
+
+// 登陆
+app.post('/login', function(req, res) {
+    res.send('登陆');
+});
+
 app.listen(8090, function() {
     console.log(`server started localhost:${port}`);
 })
